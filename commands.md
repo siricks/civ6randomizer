56ed8872b06994114507f10dcb0d3c7ef1e2aeb3
Siricks <prokopyev_sergey@inbox.ru>
2019-06-20 20:12:29 +0300

    start

---
+++ commands.md
@@ -0,0 +1,74 @@
+Запустить серв
+php bin/console server:run
+
+Создать CRUD
+php bin/console make:crud
+
+Создать класс / Добавить поле (обновить класс)
+php bin/console make:entity
+
+Создать миграцию
+php bin/console make:migration
+
+Закачать миграцию в БД
+php bin/console doctrine:migrations:migrate
+
+БД
+Создать БД
+php bin/console doctrine:database:create
+
+Удалить БД
+php bin/console doctrine:database:drop --force
+
+Фикстуры
+Создать пустой класс фикстур
+bin/console make:fixtures
+
+Загрузить фикстуры
+php bin/console doctrine:fixtures:load
+
+Создать контроллер
+php bin/console make:controller
+
+**Продакшн окружение**
+
+Чистить кэш
+php bin/console cache:clear
+
+Создать env для прода
+composer dump-env prod
+
+
+Composer commands
+Установить фикстуры
+composer require --dev doctrine/doctrine-fixtures-bundle
+
+Создать хэш пароля
+php bin/console security:encode-password
+
+Установить банд безопасности
+(https://symfony.com/doc/current/security.html#security-authorization-access-control)
+composer require symfony/security-bundle
+
+Сгенерить форму входа 
+(https://symfony.com/doc/current/security/form_login_setup.html)
+php bin/console make:auth
+
+**Сервер Бегета**
+
+Зайти и почистить кэш
+
+sshpass -p 'G0Vn0Eb@n0e' ssh webideeb:@webideeb.beget.tech "cd web-iden.ru/ && php7.2 bin/console cache:clear && exit"
+
+Зайти и провести миграцию
+
+sshpass -p 'G0Vn0Eb@n0e' ssh webideeb:@webideeb.beget.tech "cd web-iden.ru/ && php7.2 bin/console doctrine:migrations:migrate && exit"
+
+/* Запрос, который создает всем слаги из артикулов */
+UPDATE product SET product.slug=product.sku WHERE slug='';
+
+Сервер Тесоро
+
+sshpass -p '3l4rP6B7sExw' ssh root:@185.60.134.217 "cd /var/www/www-root/data/www/www.salontesoro.site/ && php bin/console doctrine:migrations:migrate && exit"
+
+sshpass -p '3l4rP6B7sExw' ssh root:@185.60.134.217 "cd /var/www/www-root/data/www/www.salontesoro.site/ && php bin/console cache:clear  && exit"
---
+++ src/Controller/LeaderController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Leader;
+use App\Form\LeaderType;
+use App\Repository\LeaderRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/leader")
+ */
+class LeaderController extends AbstractController
+{
+    /**
+     * @Route("/", name="leader_index", methods={"GET"})
+     */
+    public function index(LeaderRepository $leaderRepository): Response
+    {
+        return $this->render('leader/index.html.twig', [
+            'leaders' => $leaderRepository->findBy([], ['country' => 'ASC']),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="leader_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $leader = new Leader();
+        $form = $this->createForm(LeaderType::class, $leader);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($leader);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('leader_index');
+        }
+
+        return $this->render('leader/new.html.twig', [
+            'leader' => $leader,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="leader_show", methods={"GET"})
+     */
+    public function show(Leader $leader): Response
+    {
+        return $this->render('leader/show.html.twig', [
+            'leader' => $leader,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="leader_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Leader $leader): Response
+    {
+        $form = $this->createForm(LeaderType::class, $leader);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('leader_index', [
+                'id' => $leader->getId(),
+            ]);
+        }
+
+        return $this->render('leader/edit.html.twig', [
+            'leader' => $leader,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="leader_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Leader $leader): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$leader->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($leader);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('leader_index');
+    }
+
+    /**
+     * @Route("/random", name="leader_random", methods={"GET"})
+     */
+    public function getRandom(LeaderRepository $leaderRepository): Response
+    {
+//        return $this->render('leader/index.html.twig', [
+//            'leaders' => $leaderRepository->findBy([], ['country' => 'ASC']),
+//        ]);
+//
+//        $leaders = $leaderRepository->findAll();
+//        shuffle($leaders);
+//        $random_leader = current($leaders);
+//
+//        return $this->render('leader/index.html.twig', [
+//            'leaders' => $leaders,
+//        ]);
+    }
+}
